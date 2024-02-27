a23d2820ce8c6bdd939253bc863b7db5b0a264e7
Utsav Joshi <joshiutsav56@gmail.com>
2024-02-27 17:08:53 +0530

    added https server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
 import Router from "./routes/routes";
+import https from "https";
 
 const app = express();
+const server = https.createServer(app);
 
 app.use(cors());
 
 app.use("/", Router);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("listening on port 3000");
 });
