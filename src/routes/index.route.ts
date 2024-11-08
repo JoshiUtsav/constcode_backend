8cd5a02c00244676236b192f7843a76c0e490161
utsavjoshiNB <utsav.joshi@nextbill.io>
2024-11-08 13:09:44 +0530

    feat: added health route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,13 @@
-import { Router } from "express";
-import authRouter from "@/routes/auth.route";
+import { Router } from 'express';
+import authRouter from '@/routes/auth.route';
+import ApiResponse from '@/utils/ApiResponse.utils';
 
 const router = Router();
 
-router.use("/auth", authRouter);
+router.use('/auth', authRouter);
+
+router.route('/health').get(function (req, res) {
+  return res.status(200).json(new ApiResponse(200, null, 'Server up and running 🎉🎉'));
+});
 
 export default router;
